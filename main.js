70f188e0ac3377626e1b8a29de84e75c3ca34a03
Messali <lanymale33@gmail.com>
2021-12-27 17:48:41 +0100

    nav responsive

--- main.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector('.link')
-const button = document.querySelector('.navbar-toggler')
-
-button.addEventListener('click', ()=>{
-  nav.classList.toggle('collapse')
-})
